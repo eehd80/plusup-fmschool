ea9b6f70a2657b73af0d735deb9edc704a7619d3
eehd80 <eehd80yul@gmail.com>
2022-05-06 05:30:34 +0900

    nav responsive

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,35 +1,35 @@
-// $(function () {
-//     //gnb
-//     let $gnb = $("#gnb"),
-//         $gnbDepth1 = $gnb.find(".gnb-1dep > li"),
-//         $gnbBg = $gnb.find(".gnb-bg");
+$(function () {
+    //gnb
+    let $gnb = $("#gnb"),
+        $gnbDepth1 = $gnb.find(".gnb-1dep > li"),
+        $gnbBg = $gnb.find(".gnb-bg");
 
-//     //초기화
-//     $gnbDepth1.removeClass("on");
+    //초기화
+    $gnbDepth1.removeClass("on");
 
-//     $gnbDepth1.on("focusin mouseenter", function () {
-//         $(this).addClass("on");
+    $gnbDepth1.on("focusin mouseenter", function () {
+        $(this).addClass("on");
 
-//         let depth2Height = $(this).children(".gnb-2dep").innerHeight();
+        let depth2Height = $(this).children(".gnb-2dep").innerHeight();
 
-//         $gnbBg.stop().animate(
-//             {
-//                 height: depth2Height + "px",
-//             },
-//             300,
-//             "easeOutExpo",
-//         );
-//     });
-//     $gnbDepth1.on("focusout mouseleave", function () {
-//         $(this).removeClass("on");
-//         $gnbBg.stop().animate(
-//             {
-//                 height: "0",
-//             },
-//             {
-//                 duration: 600,
-//                 easing: "easeOutBack",
-//             },
-//         );
-//     });
-// });
+        $gnbBg.stop().animate(
+            {
+                height: depth2Height + "px",
+            },
+            300,
+            "easeOutExpo",
+        );
+    });
+    $gnbDepth1.on("focusout mouseleave", function () {
+        $(this).removeClass("on");
+        $gnbBg.stop().animate(
+            {
+                height: "0",
+            },
+            {
+                duration: 600,
+                easing: "easeOutBack",
+            },
+        );
+    });
+});
